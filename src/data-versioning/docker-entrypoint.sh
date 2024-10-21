c1f21ac0d73071ac275ab1a3d28038cbebb9bb69
hqhanqizhq <hanqizenghannanana@gmail.com>
2024-10-20 20:31:32 -0400

    update versioning container gcp project and buckets

--- src/data-versioning/docker-entrypoint.sh
+++ src/data-versioning/docker-entrypoint.sh
@@ -8,6 +8,6 @@ mkdir -p /mnt/gcs_bucket
 gcsfuse --key-file=$GOOGLE_APPLICATION_CREDENTIALS $GCS_BUCKET_NAME /mnt/gcs_data
 echo 'GCS bucket mounted at /mnt/gcs_data'
 mkdir -p /app/llm_training_data
-mount --bind /mnt/gcs_data/llm_training_data /app/llm_training_data
-
+cp $CSV_SOURCE_PATH $CSV_DEST_PATH
+echo "Copied $CSV_SOURCE_PATH to $CSV_DEST_PATH"
 pipenv shell
