179d147e79fc5af3e884cfed0703f336856e5cbb
cassieda <cdai@g.harvard.edu>
2024-11-17 11:54:48 -0500

    add food detection to api services

--- src/food-detection/gemini-object-detection.py
+++ src/food-detection/gemini-object-detection.py
@@ -8,7 +8,7 @@ def identify_food_gemini(image_path, api_key):
 
     model = genai.GenerativeModel("gemini-1.5-flash")
     result = model.generate_content(
-        [myfile, "\n\n", "Give me the name of food items shown in this image. Not a full sentense."]
+        [myfile, "\n\n", "Identify the list of food items shown in this image. (Example Output1: ['pear','apple','salt', ...]). If no food is found in the image, output ['None']. (Example Output2: ['None'])"]
     )
     return result.text
 
@@ -16,12 +16,13 @@ def main():
     if len(sys.argv) != 2:
         print("Usage: python food_item_identifier.py <image_path>")
         sys.exit(1)
+    else:
+        image_path = sys.argv[1]
+        api_key = os.getenv("GEMINI_API_KEY")
 
-    image_path = sys.argv[1]
-    api_key = os.getenv("GEMINI_API_KEY")
+        result = identify_food_gemini(image_path, api_key)
+        print(result)
 
-    result = identify_food_gemini(image_path, api_key)
-    print(result)
 
 if __name__ == "__main__":
     main()
