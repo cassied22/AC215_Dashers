1f758e031083c641f169a910b626565b094d13b6
cassieda <cdai@g.harvard.edu>
2024-12-10 08:09:56 -0500

    remove extra files

--- src/ml-pipeline/docker-entrypoint.sh
+++ src/ml-pipeline/docker-entrypoint.sh
@@ -6,8 +6,7 @@ echo "Container is running!!!"
 gcloud auth activate-service-account --key-file $GOOGLE_APPLICATION_CREDENTIALS
 # Set GCP Project Details
 gcloud config set project $GCP_PROJECT
-# Configure GCR
-gcloud auth configure-docker gcr.io -q
+
 
 args="$@"
 echo $args
