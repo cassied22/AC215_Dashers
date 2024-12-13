3383cc8d79991f65d4342541118fefe0feab3676
Chris Wang <yuxinw05@gmail.com>
2024-12-12 22:08:19 -0500

    Update on CD

--- src/deployment/deploy-k8s-update.sh
+++ src/deployment/deploy-k8s-update.sh
@@ -1,2 +1,4 @@
 ansible-playbook deploy-register-docker-images.yml -i inventory.yml
-ansible-playbook update-k8s-containers.yml -i inventory-prod.yml
+ansible-playbook deploy-k8s-create-cluster.yml -i inventory.yml
+ansible-playbook deploy-k8s-setup-containers.yml -i inventory-prod.yml
+# ansible-playbook update-k8s-containers.yml -i inventory-prod.yml
